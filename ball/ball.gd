3d4c7964c127ceaccbe52aa33fcd32f6fbc7801d
EscapESC <radavasik@gmail.com>
2025-04-15 16:06:34 +0200

    Added: Simple ball shooting

--- ball/ball.gd
+++ ball/ball.gd
@@ -3,8 +3,23 @@ extends RigidBody2D
 var ALLOW_SHOOT:bool = true;
 var CAN_SHOOT:bool = false;
 
-func _ready() -> void:
-	apply_impulse(Vector2(100,100),Vector2(0,0));
-	
+var shoot_pos_start:Vector2;
+var shoot_pos_end:Vector2;
+var SHOOTING:bool = false;
+
+var range:int = 50;
+@export var forceMultiplayer = 50;
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("shoot") and get_global_mouse_position().x-self.position.x < range and get_global_mouse_position().x-self.position.x > -range and get_global_mouse_position().y-self.position.y < range and get_global_mouse_position().y-self.position.y > -range and self.linear_velocity < Vector2(1,1):
+		shoot_pos_start = get_global_mouse_position();
+		print("Starting shooting!")
+		SHOOTING = true;
+	elif event.is_action_released("shoot") and SHOOTING:
+		shoot_pos_end = get_global_mouse_position();
+		apply_force((shoot_pos_start-shoot_pos_end)*forceMultiplayer,Vector2(0,0));
+		print("Shooting!");
+		SHOOTING = false;
+		
 func _physics_process(delta: float) -> void:
-	pass
+	print(self.linear_velocity);
