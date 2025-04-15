e4057e477edd8987c25889ecc3fe088859f99769
EscapESC <radavasik@gmail.com>
2025-04-15 17:12:39 +0200

    Fixed: Ball shoot line.

--- ball/ball.gd
+++ ball/ball.gd
@@ -7,24 +7,22 @@ var shoot_pos_start:Vector2;
 var shoot_pos_end:Vector2;
 var SHOOTING:bool = false;
 
-var range:int = 50;
-@export var forceMultiplayer = 50;
+var range:int = 10;
+@export var forceMultiplayer = 500;
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot") and get_global_mouse_position().x-self.position.x < range and get_global_mouse_position().x-self.position.x > -range and get_global_mouse_position().y-self.position.y < range and get_global_mouse_position().y-self.position.y > -range and self.linear_velocity < Vector2(1,1):
 		shoot_pos_start = get_global_mouse_position();
-		print("Starting shooting!")
 		SHOOTING = true;
 	elif event.is_action_released("shoot") and SHOOTING:
 		shoot_pos_end = get_global_mouse_position();
-		apply_force((shoot_pos_start-shoot_pos_end)*forceMultiplayer,Vector2(0,0));
-		print("Shooting!");
+		apply_force(Vector2((shoot_pos_start-shoot_pos_end).x*forceMultiplayer,(shoot_pos_start-shoot_pos_end).y*forceMultiplayer),Vector2(0,0));
+		print(shoot_pos_start-shoot_pos_end);
 		SHOOTING = false;
 		
 func _process(delta: float) -> void:
-	print(self.linear_velocity);
 	queue_redraw()
 	
 func _draw() -> void:
 	if SHOOTING == true:
-		draw_line(self.position, get_global_mouse_position(), Color.WHITE, 5.0)
+		draw_line(to_local(self.position), get_local_mouse_position(), Color.WHITE, 5.0)
