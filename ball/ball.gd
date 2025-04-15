3461f375035531f9142b6ceda5a74536845e129d
EscapESC <radavasik@gmail.com>
2025-04-15 18:33:13 +0200

    Fixed: Physics problems

--- ball/ball.gd
+++ ball/ball.gd
@@ -7,17 +7,16 @@ var shoot_pos_start:Vector2;
 var shoot_pos_end:Vector2;
 var SHOOTING:bool = false;
 
-var range:int = 10;
-@export var forceMultiplayer = 500;
+var range:int = 500;
+@export var forceMultiplayer = 5;
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("shoot") and get_global_mouse_position().x-self.position.x < range and get_global_mouse_position().x-self.position.x > -range and get_global_mouse_position().y-self.position.y < range and get_global_mouse_position().y-self.position.y > -range and self.linear_velocity < Vector2(1,1):
+	if event.is_action_pressed("shoot") and get_global_mouse_position().x-self.position.x < range and get_global_mouse_position().x-self.position.x > -range and get_global_mouse_position().y-self.position.y < range and get_global_mouse_position().y-self.position.y > -range and self.linear_velocity.x < 1 and linear_velocity.y < 1:
 		shoot_pos_start = get_global_mouse_position();
 		SHOOTING = true;
 	elif event.is_action_released("shoot") and SHOOTING:
 		shoot_pos_end = get_global_mouse_position();
-		apply_force(Vector2((shoot_pos_start-shoot_pos_end).x*forceMultiplayer,(shoot_pos_start-shoot_pos_end).y*forceMultiplayer),Vector2(0,0));
-		print(shoot_pos_start-shoot_pos_end);
+		apply_force(Vector2(clamp((shoot_pos_start-shoot_pos_end).x*forceMultiplayer,-10000000,10000000),clamp((shoot_pos_start-shoot_pos_end).y*forceMultiplayer,-100000,100000)),Vector2(0,0));
 		SHOOTING = false;
 		
 func _process(delta: float) -> void:
