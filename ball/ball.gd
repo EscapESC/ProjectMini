56c8c0af66cb3a712608fb14d63f4f73e8977f71
EscapESC <radavasik@gmail.com>
2025-04-25 13:20:35 +0200

    Added: Ball arrow.

--- ball/ball.gd
+++ ball/ball.gd
@@ -12,13 +12,18 @@ var maxShootingStrengthPx:int = 500;
 var range:int = 50;
 @export var forceMultiplayer:float = 1;
 
-@export var powerMeter:TextureProgressBar
+@export var powerMeter:TextureProgressBar;
+@export var Arrow:Sprite2D;
+
+func _ready() -> void:
+	Arrow.visible = false;
 
 func _unhandled_input(event: InputEvent) -> void:
 	if (event.is_action_pressed("shoot") or event is InputEventScreenTouch) and get_global_mouse_position().x-self.position.x < range and get_global_mouse_position().x-self.position.x > -range and get_global_mouse_position().y-self.position.y < range and get_global_mouse_position().y-self.position.y > -range and linear_velocity.length() < 10:
 		shoot_pos_start = self.position
 		SHOOTING = true;
 		powerMeter.visible = true;
+		Arrow.visible = true;
 	elif (event.is_action_released("shoot") or event is InputEventScreenTouch) and SHOOTING:
 		shoot_pos_end = get_global_mouse_position();
 		var distance = shoot_pos_end.distance_to(shoot_pos_start);
@@ -33,12 +38,16 @@ func _unhandled_input(event: InputEvent) -> void:
 		apply_impulse(Vector2(forceX,forceY));
 		
 		SHOOTING = false;
+		Arrow.visible = false;
 		powerMeter.visible = false;
 func _process(delta: float) -> void:
 	queue_redraw()
 	
 func _draw() -> void:
 	if SHOOTING == true:
+		Arrow.rotation = (get_local_mouse_position() - to_local(self.position)).normalized().angle() - PI/2;
+		Arrow.scale = Vector2(min(to_local(self.position).distance_to(get_local_mouse_position())/maxShootingStrengthPx,1)*5,min(to_local(self.position).distance_to(get_local_mouse_position())/maxShootingStrengthPx,1)*5);
+		
 		if to_local(self.position).distance_to(get_local_mouse_position()) > maxShootingStrengthPx:
 			var start = to_local(self.position)
 			var dir = (get_local_mouse_position() - start).normalized()
